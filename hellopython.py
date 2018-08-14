951e990f32229f102a35981dd8815b4e6b0aa158
xianqv <1239331734@qq.com>
2018-08-15 00:02:18 +0800

    deploy python derectory

---
+++ hellopython.py
@@ -0,0 +1,7 @@
+##pyhton 中可随时修改变量的值 但是python始终记录最新的值
+##python 运行发生错误时 会提供一个traceback 来指出发生错误的地方
+print("hello ,python!")
+message = "hello world"
+print(message)
+message = "this is new message"
+print(message)
