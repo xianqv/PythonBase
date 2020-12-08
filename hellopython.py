9fcbd20a07b86ede32261bb8d13f9db7d581ba87
haigang <1239331734@qq.com>
2020-12-08 15:19:10 +0800

    some changes

--- hellopython.py
+++ hellopython.py
@@ -1,7 +1,19 @@
+import itertools
 ##pyhton 中可随时修改变量的值 但是python始终记录最新的值
 ##python 运行发生错误时 会提供一个traceback 来指出发生错误的地方
-print("hello ,python!")
-message = "hello world"
-print(message)
-message = "this is new message"
-print(message)
+
+if __name__ == "__main__":
+    print("hello ,python!")
+#**题目:**有四个数字:1、2、3、4，能组成多少个互不相同且无重复数字的三位数?各是多少?
+numberlist=[1,2,3,4]
+total =0
+for i in numberlist:
+    for j in numberlist:
+        for k in numberlist:
+            if((i!=j)and(j!=k)and(k!=i)):
+                print(i,j,k)
+                total+=1
+print (total)
+## 使用itertools  的permutations即可
+for i in itertools.permutations(numberlist,3):
+    print(i)
